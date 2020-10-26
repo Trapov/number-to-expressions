082d94c8381357c9e55883bf27c87a6f00b16567
dmitriy afonin <klajaa1@gmail.com>
2020-10-26 17:02:44 +0500

    Fixes of arguments parsing.

--- src/NumberToExpressions.Cli/Arguments.cs
+++ src/NumberToExpressions.Cli/Arguments.cs
@@ -6,11 +6,11 @@
 
   public sealed class Arguments {
 
-    [Value(0)]
+    [Value(0, Required = true, HelpText = "Number to find expressions")]
     public Double Number { get; private set; }
 
-    [Option(longName: "complexity", shortName: 'v', Required = false)]
-    public UInt32 Complexity { get; private set; }
+    [Option(longName: "complexity", shortName: 'c', Default = 4u, Required = false)]
+    public UInt32 Complexity { get; set; }
 
     [Option(longName: "seed", shortName: 's', Required = false)]
     public Int32 Seed { get; private set; }
